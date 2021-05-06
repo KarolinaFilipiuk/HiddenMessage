05d6b270c14ea3d6b3ef34fef372fe1299411f54
KarolinaFilipiuk <k.filipiuk18@gmail.com>
2021-05-06 16:19:53 +0200

    upload a few versions of code

--- main.js
+++ main.js
@@ -8,7 +8,38 @@ const messages = ["wyjechałam na zawsze", "piękna pora roku", "fajny masz styl
 // version 2
 upperCasePasswords = passwords.map(password => password.toUpperCase())
 
-const showMessage = (e) => {
+const loadingData = () => {
+    let i = 0;
+
+    function move() {
+        if (i == 0) {
+            i = 1;
+            var bar = document.getElementById("bar");
+            var width = 1;
+            var id = setInterval(frame, 20);
+
+            function frame() {
+                if (width >= 100) {
+                    clearInterval(id);
+                    i = 0;
+                } else {
+                    width++;
+                    bar.style.width = width + "%";
+                }
+            }
+        }
+    }
+
+    const progress = document.createElement("div");
+    const bar = document.createElement("div");
+    progress.id = "progress";
+    bar.id = "bar";
+    progress.appendChild(bar);
+    document.body.insertBefore(progress, document.getElementById("welcome"));
+    move();
+}
+
+async function showMessage(e) {
     // console.log(e.target.value);
     // console.log(this.value);
 
@@ -31,14 +62,31 @@ const showMessage = (e) => {
     //     }
     // })
 
+
+
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+    const deleteProgressBar = () => document.body.removeChild(document.getElementById("progress"))
+
     // version 3
+    // div.textContent = "";
+    // passwords.forEach((password, index) => {
+    //     if (password.toUpperCase() === e.target.value.toUpperCase()) {
+    //         div.textContent = messages[index];
+    //         e.target.value = "";
+    //     }
+    // })
+
+    // version 4 (last)
     div.textContent = "";
-    passwords.forEach((password, index) => {
-        if (password.toUpperCase() === e.target.value.toUpperCase()) {
-            div.textContent = messages[index];
+    for (let i = 0; i < passwords.length; i++) {
+        if (passwords[i].toUpperCase() === e.target.value.toUpperCase()) {
+            loadingData();
+            await sleep(2000);
+            div.textContent = messages[i];
             e.target.value = "";
+            deleteProgressBar();
         }
-    })
+    }
 }
 
 input.addEventListener("input", showMessage)
